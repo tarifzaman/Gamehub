c79091bfb6949c4dd05c010ccd1960f64ef0ea55
Tarif Zaman <tarifzaman69@gmail.com>
2025-12-29 20:06:08 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound";
-import MyProfile from "./pages/Myprofile";
+import MyProfile from "./pages/MyProfile";
 import GameDetails from "./pages/GameDetails";
 import Wishlist from "./pages/Wishlist";
 import UpdateProfile from "./pages/UpdateProfile";
